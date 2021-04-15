0731cb84c790e44f5271448bf5b1ee2f7fcffb4e
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2021-04-15 23:03:22 +0200

    Add unit tests for name schema derivation for Option

--- src/test/scala/names/GenericNameSchemaDerivation.scala
+++ src/test/scala/names/GenericNameSchemaDerivation.scala
@@ -21,6 +21,13 @@ class GenericNameSchemaDerivation
     forAll { (a: List[Int]) => encoder.encode(a) shouldBe expected }
   }
 
+  test("Name schema for option of int should be derivable") {
+    val encoder = namesSchemaEncoder[Option[Int]]
+    val expected = OptionSchema("", Some(IntSchema("")))
+    encoder.encode shouldBe expected
+    forAll { (a: Option[Int]) => encoder.encode(a) shouldBe expected }
+  }
+
   test("Name schema for list of products should be derivable") {
     case class C(i: Int, s: String, b: Boolean)
     implicit val arbitrary = implicitly[Arbitrary[C]]
@@ -40,6 +47,27 @@ class GenericNameSchemaDerivation
     )
     encoder.encode shouldBe expected
     forAll { (a: List[C]) => encoder.encode(a) shouldBe expected }
+  }
+
+  test("Name schema for option of product should be derivable") {
+    case class C(i: Int, s: String, b: Boolean)
+    implicit val arbitrary = implicitly[Arbitrary[C]]
+    val encoder = namesSchemaEncoder[Option[C]]
+    val expected = OptionSchema(
+      "",
+      Some(
+        ProductSchema(
+          "",
+          List(
+            IntSchema("i"),
+            StringSchema("s"),
+            BooleanSchema("b")
+          )
+        )
+      )
+    )
+    encoder.encode shouldBe expected
+    forAll { (a: Option[C]) => encoder.encode(a) shouldBe expected }
   }
 
   test("Name schema for list of coproducts should be derivable") {
@@ -63,5 +91,28 @@ class GenericNameSchemaDerivation
     )
     encoder.encode shouldBe expected
     forAll { (a: List[T]) => encoder.encode(a) shouldBe expected }
+  }
+
+  test("Name schema for option of coproduct should be derivable") {
+    sealed trait T
+    case class A(i: Int) extends T
+    case class B(s: String) extends T
+
+    implicit val arbitrary = implicitly[Arbitrary[T]]
+    val encoder = namesSchemaEncoder[Option[T]]
+    val expected = OptionSchema(
+      "",
+      Some(
+        CoproductSchema(
+          "",
+          List(
+            ProductSchema("A", List(IntSchema("i"))),
+            ProductSchema("B", List(StringSchema("s")))
+          )
+        )
+      )
+    )
+    encoder.encode shouldBe expected
+    forAll { (a: Option[T]) => encoder.encode(a) shouldBe expected }
   }
 }
